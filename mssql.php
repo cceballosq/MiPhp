370bc7e5597936816d251a526185c9fec43a0a3a
Agustin Santana <agustin.santana@gmail.com>
2018-01-29 13:11:52 -0600

    mssql
    conexion de mssql

---
+++ mssql.php
@@ -0,0 +1,28 @@
+<?php
+//************************************************************************************************
+//* mssql
+//************************************************************************************************
+$server = '10.10.40.104';
+$sistema_user = 'usrinfo';
+$sistema_pwd = 'Uinfo15Egr';
+$Database = 'DBPRUEBAS';
+
+    $conexion = mssql_connect($server, $sistema_user, $sistema_pwd);
+    
+    if(!$conexion)
+    {
+        $error = error_get_last();
+        $error = $error["message"];        
+        die("No se puede conectar al servidor (Disculpe las molestias)- $error");
+    }
+
+mssql_select_db($Database, $conexion) or die("Imposible seleccionar base de datos."); 
+
+
+
+
+//************************************************************************************************
+//* sql_server
+//************************************************************************************************
+
+?>
